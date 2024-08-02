b38061c70acaa1b53f93774dc707d551d2b6a604
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-08-02 16:19:38 +0300

    create pages and put them in router-provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
+import LoginPage from "./pages/login";
+import DashboardPage from "./pages/dashboard";
 
 function App() {
-  return <>Hello World</>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      children: [
+        {
+          index: true,
+          element: <LoginPage />,
+        },
+        {
+          path: "dashboard",
+          element: <DashboardPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
